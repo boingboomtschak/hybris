5be1c47e87b5b0cc9bd6b901436f17d76bfd5a77
Devon <36770835+boingboomtschak@users.noreply.github.com>
2022-11-30 19:05:02 -0800

    Putting BFS together, still need to create/copy buffer for stop, create pipeline/binding group layout, translate kernel(s), and run loop

--- www/hybris.js
+++ www/hybris.js
@@ -3,6 +3,7 @@ class Hybris {
     constructor() {
         this.adapter = null;
         this.device = null;
+        this.bfsGraph = null;
     }
     async initializeGPU() {
         console.log("Initializing Hybris...");
@@ -153,16 +154,142 @@ class Hybris {
         else
             console.log(`${N} results checked, incorrect!`);
     }
+    async runBFS() {
+        if (this.adapter === null || this.device === null) {
+            console.log("Hybris not initialized!");
+            return; 
+        }
+
+        const MAX_THREADS_PER_BLOCK = 512;
+        let no_of_nodes = 0;
+        let edge_list_size = 0;
+        let source = 0;
+
+        console.log("Retrieving and processing data/graph65536...");   
+        const response = await fetch('/data/graph65536.txt');
+        let text = await response.text();
+        text = text.split(/\r?\n/);
+
+        // Reading graph nodes and setting up graph mask/visited arrays
+        no_of_nodes = parseInt(text.shift());
+        const h_graph_nodes_starting = new Uint32Array(no_of_nodes);
+        const h_graph_nodes_no_of_edges = new Uint32Array(no_of_nodes);
+        const h_graph_mask = new Uint8Array(no_of_nodes);
+        const h_updating_graph_mask = new Uint8Array(no_of_nodes);
+        const h_graph_visited = new Uint8Array(no_of_nodes);
+        for (let i = 0; i < no_of_nodes; i++) {
+            let line = text.shift().split(" ");
+            h_graph_nodes_starting[i] = line[0];
+            h_graph_nodes_no_of_edges[i] = line[1];
+            h_graph_mask[i] = 0;
+            h_updating_graph_mask[i] = 0;
+            h_graph_visited[i] = 0;
+        }
+        
+        // Reading graph source
+        text.shift();
+        let source_line = text.shift();
+        source = (isNaN(source_line)) ? source : parseInt(source_line);
+        
+        // Reading graph edge list size
+        text.shift();
+        let edge_list_size_line = text.shift();
+        edge_list_size = (isNaN(edge_list_size_line)) ? edge_list_size : parseInt(edge_list_size_line);
+        
+        // Reading graph edges
+        let h_graph_edges = new Uint32Array(edge_list_size);
+        for (let i = 0; i < edge_list_size; i++) {
+            let line = text.shift().split(" ");
+            h_graph_edges[i] = line[0];
+        }
+        console.log("Processed data/graph65536.");
+
+        let num_of_blocks = 1;
+        let num_of_threads_per_block = no_of_nodes;
+        if (no_of_nodes > MAX_THREADS_PER_BLOCK) {
+            num_of_blocks = Math.ceil(no_of_nodes/MAX_THREADS_PER_BLOCK);
+            num_of_threads_per_block = MAX_THREADS_PER_BLOCK;
+        }
+
+        h_graph_mask[source] = 1;
+        h_graph_visited[source] = 1;
+
+        // Creating device buffers
+        console.log("Creating buffers for graph and copying to device memory...");
+        const d_graph_nodes_starting = this.device.createBuffer({
+            size: no_of_nodes*Uint32Array.BYTES_PER_ELEMENT,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        });
+        this.device.queue.writeBuffer(d_graph_nodes_starting, 0, h_graph_nodes_starting);
+        const d_graph_nodes_no_of_edges = this.device.createBuffer({
+            size: no_of_nodes*Uint32Array.BYTES_PER_ELEMENT,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        });
+        this.device.queue.writeBuffer(d_graph_nodes_no_of_edges, 0, h_graph_nodes_no_of_edges);
+        const d_graph_edges = this.device.createBuffer({
+            size: edge_list_size*Uint32Array.BYTES_PER_ELEMENT,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        });
+        this.device.queue.writeBuffer(d_graph_edges, 0, h_graph_edges);
+        const d_graph_mask = this.device.createBuffer({
+            size: no_of_nodes*Uint8Array.BYTES_PER_ELEMENT,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        });
+        this.device.queue.writeBuffeR(d_graph_mask, 0, h_graph_mask);
+        const d_updating_graph_mask = this.device.createBuffer({
+            size: no_of_nodes*Uint8Array.BYTES_PER_ELEMENT,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        });
+        this.device.queue.writeBuffer(d_updating_graph_mask, 0, h_updating_graph_mask);
+        const d_graph_visited = this.device.createBuffer({
+            size: no_of_nodes*Uint8Array.BYTES_PER_ELEMENT,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+        });
+        this.device.queue.writeBuffer(d_graph_visited, 0, h_graph_visited);
+
+        // Create host and device buffer for result
+        console.log("Creating host and device memory for result...");
+        const h_cost = new Uint32Array(no_of_nodes);
+        for (let i = 0; i < no_of_nodes; i++) 
+            h_cost[i] = -1;
+        h_cost[source] = 0;
+        const d_cost = this.device.createBuffer({
+            size: no_of_nodes*Uint32Array.BYTES_PER_ELEMENT,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC
+        });
+        this.device.queue.writeBuffer(d_cost, 0, h_cost);
+        const d_cost_map = this.device.createBuffer({
+            size: no_of_nodes*Uint32Array.BYTES_PER_ELEMENT,
+            usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST
+        });
+
+        // Set up buffer(s) for stop/d_over 
+
+        // Create pipeline/binding group layout/kernel
+
+        console.log("Running kernels...");
+        let stop;
+        do {
+            stop = false;
+            
+            const commandEncoder = this.device.createCommandEncoder();
+            const computePassEncoder = commandEncoder.beginComputePass();
+            // dispatch kernels
+            computePassEncoder.end();
+            this.device.queue.submit([commandEncoder.finish()]);
+
+        } while (stop);
+    }
     async runBenchmark(benchmark) {
         console.log(`Running ${benchmark}...`);
         switch(benchmark) {
             case "vector-add": await this.runVectorAdd(); break;
-            case "bfs": console.log("Not yet implemented!"); break;
+            case "bfs": await this.runBFS(); break;
             case "gaussian": console.log("Not yet implemented!"); break;
             case "particlefilter": console.log("Not yet implemented!"); break;
             default: console.log("Unknown benchmark!"); break;
         }
-        console.log(`Finished ${benchmark}`);
+        console.log(`Finished ${benchmark}.`);
         console.log("----------------");
     }
 }
